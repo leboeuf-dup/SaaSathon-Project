152a8bf9a224ded219ce49c1535695e7691f0395
leboeuf-dup <liamldup@gmail.com>
2026-05-08 20:55:42 +1200

    First project as a test

---
+++ first-app/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        SaaSathon MVP
+      </h1>
+
+      <p className="mt-4 text-xl">
+        My first Next.js app. "Hello World!!!"
+      </p>
+    </main>
+  )
+}
