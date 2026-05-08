8b19ffe2038439646083a9c0b611cf5318364865
leboeuf-dup <liamldup@gmail.com>
2026-05-09 09:15:32 +1200

    Changes while deployed

--- first-app/app/page.tsx
+++ first-app/app/page.tsx
@@ -1,13 +1,9 @@
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center">
+    <main className="min-h-screen flex items-center justify-center">
       <h1 className="text-5xl font-bold">
-        SaaSathon MVP
+        SaaSathon is working 🚀
       </h1>
-
-      <p className="mt-4 text-xl">
-        My first Next.js app. "Hello World!!!"
-      </p>
     </main>
   )
 }
