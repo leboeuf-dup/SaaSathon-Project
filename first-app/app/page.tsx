f467e6472c140d6d5610a03af0137225172b8f58
leboeuf-dup <liamldup@gmail.com>
2026-05-09 15:01:45 +1200

    Deleting unused files

--- first-app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-5xl font-bold">
-        SaaSathon is working 🚀
-      </h1>
-    </main>
-  )
-}
